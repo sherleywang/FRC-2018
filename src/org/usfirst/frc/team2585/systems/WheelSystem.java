320e4387c31d42c0693bc09d6d916656d99712ae
Ryan Knightly <reknightly@gmail.com>
2018-01-26 14:57:14 -0600

    Use gyroscope readings to calibrate drivetrain motor speeds

--- src/org/usfirst/frc/team2585/systems/WheelSystem.java
+++ src/org/usfirst/frc/team2585/systems/WheelSystem.java
@@ -1,25 +1,31 @@
 package org.usfirst.frc.team2585.systems;
 
-import org.impact2585.lib2585.Drivetrain;
 import org.usfirst.frc.team2585.robot.Environment;
 import org.usfirst.frc.team2585.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This system controls the drivetrain of the robot
  */
 public class WheelSystem extends RobotSystem {
-	SpeedController rightDrive;
-	SpeedController leftDrive;
+	private SpeedController rightDrive;
+	private SpeedController leftDrive;
 	
-	Drivetrain drive;
+	private ADXRS450_Gyro gyro;
 	
-	private final double leftPercentage = 0.65;
-	private final double rightPercentage = 0.65;
-
+	private double leftPercentage = 0.65;
+	private double rightPercentage = 0.65;
+	
+	private final double angleChangeWeight = 1.0;
+	private double previousAngle;
+	private double currentAngle;
+	
+	private boolean isMovingForward = false;
 	
 	/* (non-Javadoc)
 	 * @see org.usfirst.frc.team2585.systems.Initializable#init(org.usfirst.frc.team2585.Environment)
@@ -30,16 +36,16 @@ public class WheelSystem extends RobotSystem {
 
 		leftDrive = new Spark(RobotMap.LEFT_DRIVE_MOTOR);
 		rightDrive = new Spark(RobotMap.RIGHT_DRIVE_MOTOR);
-				
-		DifferentialDrive botDrive = new DifferentialDrive(leftDrive, rightDrive);
-
-		drive = new Drivetrain(botDrive);	
+						
+		gyro = new ADXRS450_Gyro();
+		currentAngle = gyro.getAngle();
 	}
 	
 	/**
 	 * Pass the user inputs to the drive train to run the motors the appropriate amounts
 	 */
 	public void run() {
+		calibrate();
 		driveWithRotation(input.forwardAmount(), input.rotationAmount());
 	}
 	
@@ -48,15 +54,38 @@ public class WheelSystem extends RobotSystem {
 	 * @param rotation the amount to rotate
 	 */
 	public void driveWithRotation(double forward, double rotation) {
-		drive.arcadeDriveWithPercentages(forward, rotation, leftPercentage, rightPercentage);
+		if (forward > 0 && rotation == 0) {
+			isMovingForward = true;
+		} else {
+			isMovingForward = false;
+		}
+		
+		leftDrive.set(leftPercentage * (forward + rotation));
+		rightDrive.set(rightPercentage * (forward - rotation));
 	}
-
-	/* (non-Javadoc)
-	 * @see org.impact2585.lib2585.Destroyable#destroy()
+	
+	/**
+	 * Calibrate the right and left sides based on the gyroscope data
 	 */
-	@Override
-	public void destroy() {
-		drive.destroy();
+	private void calibrate() {
+		previousAngle = currentAngle;
+		currentAngle = gyro.getAngle();
+		SmartDashboard.putNumber("Gyro Angle", currentAngle);
+		
+		if (isMovingForward) {
+			double angleChange = currentAngle - previousAngle;
+			// Positive: veering left
+			// Negative: veering right
+			double weightedAngleChange = angleChange * angleChangeWeight;
+			if ((leftPercentage + weightedAngleChange < 1) && (rightPercentage - weightedAngleChange > 0)) {
+				leftPercentage = leftPercentage + weightedAngleChange;
+				rightPercentage = rightPercentage - weightedAngleChange;
+						
+			}
+			SmartDashboard.putNumber("RawAngleChange", angleChange);
+		}
+		SmartDashboard.putNumber("Left Percentage", leftPercentage);
+		SmartDashboard.putNumber("Right Percentage", rightPercentage);
 	}
 	
 	/* (non-Javadoc)
@@ -64,6 +93,19 @@ public class WheelSystem extends RobotSystem {
 	 */
 	@Override
 	public void stop() {
-		drive.arcadeControl(0, 0, false, false);
+		driveWithRotation(0, 0);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.impact2585.lib2585.Destroyable#destroy()
+	 */
+	@Override
+	public void destroy() {
+		if (leftDrive instanceof PWM) {
+			((PWM) leftDrive).free();
+		}
+		if (rightDrive instanceof PWM) {
+			((PWM) rightDrive).free();
+		}
 	}
 }
