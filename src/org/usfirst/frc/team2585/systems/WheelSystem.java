6df8cffc9c4c1bab2f692c52edcdb924521120dc
Ryan Knightly <reknightly@gmail.com>
2018-01-29 17:29:37 -0600

    Add derivative of error and change direction of right side

--- src/org/usfirst/frc/team2585/systems/WheelSystem.java
+++ src/org/usfirst/frc/team2585/systems/WheelSystem.java
@@ -17,13 +17,15 @@ public class WheelSystem extends RobotSystem {
 	
 	private ADXRS450_Gyro gyro;
 	
-	private double targetAngle;
+	private double targetAngle = 0.0;
 	
-	private final double DEADZONE = 0.15;
-	private final double CORRECTION_MULTIPLIER = 0.015;
+	private final double DEADZONE = 0.2;
 	
 	private final double FORWARD_MULTIPLIER = 0.65;
-	private final double ROTATION_RATE = 0.5;
+	private final double ROTATION_RATE = 4.0;
+	private final double DERIVATIVE_MULTIPLIER = 0.1;
+	private final double CORRECTION_MULTIPLIER = 0.01;
+	private final double MAX_CORRECTION = 0.6;
 	
 	public static boolean IS_TEST_SYSTEM = false;
 	
@@ -37,51 +39,65 @@ public class WheelSystem extends RobotSystem {
 
 		leftDrive = new RampedSpeedController(new Spark(RobotMap.LEFT_DRIVE_MOTOR));
 		rightDrive = new RampedSpeedController(new Spark(RobotMap.RIGHT_DRIVE_MOTOR));
-								
-		targetAngle = getGyroAngle();
+		
 		gyro = new ADXRS450_Gyro();
+		targetAngle = getGyroAngle();
 	}
 	
 	/**
 	 * Pass the user inputs to the drive train to run the motors the appropriate amounts
 	 */
 	public void run() {
-		double forward = -input.forwardAmount(); // reverse direction of driving
-		forward = (Math.abs(forward) > DEADZONE)? forward * FORWARD_MULTIPLIER : 0;
-		double rotation = input.rotationAmount();
-		rotation = (Math.abs(rotation) > DEADZONE)? rotation : 0;
-		driveWithGyro(forward, rotation);
+		double forwardInput = -input.forwardAmount(); // reverse direction of driving
+		forwardInput = (Math.abs(forwardInput) > DEADZONE)? forwardInput * FORWARD_MULTIPLIER : 0;
+		
+		double rotationInput = input.rotationAmount();
+		rotationInput = (Math.abs(rotationInput) > DEADZONE)? rotationInput : 0;
+		
+		driveWithGyro(forwardInput, rotationInput);
 	}
 	
 	/**
-	 * Drive using the gyro to drive straight
+	 * Drive using the gyro to keep track of the angle
 	 * @param forwardInput the amount to drive forward
-	 * @param rotationInput the amount to rotate
+	 * @param rotationInput the amount to adjust the targetAngle
 	 */
 	public void driveWithGyro(double forwardInput, double rotationInput) {		
 		// Adjust Target Angle
-		targetAngle -= rotationInput * ROTATION_RATE;
+		targetAngle += rotationInput * ROTATION_RATE;
 		
-		// Rotate towards target
-		double correction = (getGyroAngle() - targetAngle) * CORRECTION_MULTIPLIER;
+		// Rotate towards target angle
+		double rate = getGyroRate() * DERIVATIVE_MULTIPLIER;
+		double correction = (targetAngle - getGyroAngle() - rate) * CORRECTION_MULTIPLIER;
 		
-		arcadeDrive(forwardInput, rotationInput - correction);
+		// Min correction
+		if (Math.abs(correction) < 0.001) correction = 0;
+		// Max correction
+		if (Math.abs(correction) > MAX_CORRECTION) {
+			correction = Math.copySign(MAX_CORRECTION, correction);
+		}
+		
+		arcadeDrive(forwardInput, correction);
 		
 		if (!IS_TEST_SYSTEM) {
 			SmartDashboard.putNumber("Gyro Angle",  getGyroAngle());
-			SmartDashboard.putNumber("Gyro Rate", getGyroRate());
-			SmartDashboard.putNumber("Offset Angle", getGyroAngle() - targetAngle);
-			SmartDashboard.putNumber("Forward Amount", forwardInput);
-			SmartDashboard.putNumber("RotationAmount", rotationInput);
+			SmartDashboard.putNumber("Gyro Target Angle",  targetAngle);
+			SmartDashboard.putNumber("Gyro Rate",   getGyroAngle());
 		}
 	}
 	
+	/**
+	 * @param leftSpeed the speed to set the left side to
+	 * @param rightSpeed the speed to set the right side to
+	 */
 	private void setSideSpeeds(double leftSpeed, double rightSpeed) {
 		if (Math.abs(leftSpeed) > 1) leftSpeed = Math.copySign(1, leftSpeed);
 		if (Math.abs(rightSpeed) > 1) rightSpeed = Math.copySign(1, rightSpeed);
 		
+		// Both sides driving in same direction
+		// If wiring doesn't explicitly compensate for different sides, must make rightSpeed negative
 		leftDrive.updateWithSpeed(leftSpeed);
-		rightDrive.updateWithSpeed(-rightSpeed);
+		rightDrive.updateWithSpeed(rightSpeed);
 		
 		if (!IS_TEST_SYSTEM) {
 			SmartDashboard.putNumber("LEFT SPEED RAW", leftSpeed);
@@ -92,16 +108,26 @@ public class WheelSystem extends RobotSystem {
 		}
 	}
 	
+	/**
+	 * @param forward the amount to drive forward
+	 * @param rotation the amount to rotate
+	 */
 	public void arcadeDrive(double forward, double rotation) {
 		double leftSpeed = forward + rotation;
 		double rightSpeed = forward - rotation;
 		setSideSpeeds(leftSpeed, rightSpeed);
 	}
 	
+	/**
+	 * @return the current angle that the robot is facing from the gyroscope
+	 */
 	public double getGyroAngle() {
 		return gyro.getAngle();
 	}
 	
+	/**
+	 * @return the rate that the robot is rotating from the gyroscope
+	 */
 	public double getGyroRate() {
 		return gyro.getRate();
 	}
