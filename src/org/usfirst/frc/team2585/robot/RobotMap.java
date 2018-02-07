42ff879ca17609a5b1e6a1be3f801e7cb34cfc49
jxin31415 <33794646+jxin31415@users.noreply.github.com>
2018-02-07 17:57:53 -0600

    Add SwitchDigitalInputs to RobotMap

--- src/org/usfirst/frc/team2585/robot/RobotMap.java
+++ src/org/usfirst/frc/team2585/robot/RobotMap.java
@@ -10,4 +10,6 @@ public interface RobotMap {
 	public static final int INTAKE_MOTOR_RIGHT = 4;
 	public static final int INTAKE_MOTOR_LEFT = 5;
 	public static final int CLIMB_MOTOR = 6;
+	public static final int LIMIT_SWITCH_TOP = 7;
+	public static final int LIMIT_SWITCH_BOTTOM = 8;
 }
